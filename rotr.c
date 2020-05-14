85781277021f139c3a7322d616082156131487ed
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-14 14:36:49 +0000

    Fix nodes connections

--- rotr.c
+++ rotr.c
@@ -9,11 +9,14 @@ void rotr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 	(void)line_number;
+
 	if (!stack || !*stack || !(*stack)->next)
 		return;
+
 	temp = *stack;
 	while ((*stack)->next)
-		*stack = (*stack)->next;
+		(*stack) = (*stack)->next;
+
 	(*stack)->next = temp;
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
